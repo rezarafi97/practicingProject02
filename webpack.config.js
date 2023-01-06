6da8899d94e182acedb5439404819c147ba9d76b
Reza <72236133+rezarafi97@users.noreply.github.com>
2023-01-06 20:03:12 +0330

    Delete webpack.config.js

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require("path");
-module.exports = {
-    mode: "development",
-    entry: "./src/app/js/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist")
-    }
-};
